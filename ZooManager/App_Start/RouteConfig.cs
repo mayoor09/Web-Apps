4aea8d286eceb869ea47c74a33a7e196b717b81b
Mayur Ghodekar <63019990+mayoor09@users.noreply.github.com>
2021-08-27 01:20:31 -0500

    Add files via upload

---
+++ ZooManager/App_Start/RouteConfig.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace ASPDemo
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+        /*    routes.MapRoute(
+            name: "About",
+            url: "URL/demo/{id}",
+            defaults: new { controller = "Details", action = "Details", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+            name: "Empployee",
+            url: "{controller}/{action}/{id}",
+            defaults: new { controller = "Employee", action = "index", id = UrlParameter.Optional }
+            );
+        */
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+
+
+        }
+
+    }
+}
